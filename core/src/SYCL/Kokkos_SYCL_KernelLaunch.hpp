058e94f3e9682132ff53701e8e9b6e0ca8aa11d5
Nevin Liber <nliber@anl.gov>
2020-09-29 16:37:18 +0000

    in sycl_launch, only pass the functor (and not the driver, which also contains the policy) to cgh.parallel_for on the off chance it might not be trivially copyable, as it is not needed inside the parallel_for loop itself.  Also less to pass to the kernel.
    Added more local variables to make debugging easier (they get
    optimized out on production builds).

--- core/src/SYCL/Kokkos_SYCL_KernelLaunch.hpp
+++ core/src/SYCL/Kokkos_SYCL_KernelLaunch.hpp
@@ -17,28 +17,29 @@ void sycl_launch_bind(Driver tmp, cl::sycl::handler& cgh) {
       cl::sycl::range<1>(tmp.m_policy.end() - tmp.m_policy.begin()), tmp);
 }
 
-
 template <class Driver>
 void sycl_launch(const Driver driver) {
-  isTriviallyCopyable<Driver>();
   isTriviallyCopyable<decltype(driver.m_functor)>();
-  driver.m_policy.space().impl_internal_space_instance()->m_queue->wait();
-#ifndef SYCL_USE_BIND_LAUNCH
-  driver.m_policy.space().impl_internal_space_instance()->m_queue->submit(
-      [&](cl::sycl::handler& cgh) {
-        cgh.parallel_for(
-            cl::sycl::range<1>(driver.m_policy.end() - driver.m_policy.begin()),
-            [=](cl::sycl::item<1> item) {
-              int id = item.get_linear_id();
-              driver.m_functor(id);
-            });
-      });
-  driver.m_policy.space().impl_internal_space_instance()->m_queue->wait();
-#else
-  driver.m_policy.space().impl_internal_space_instance()->m_queue->submit(
-      std::bind(Kokkos::Experimental::Impl::sycl_launch_bind<Driver>, driver,
-                std::placeholders::_1));
-#endif
+  auto& policy  = driver.m_policy;
+  auto& functor = driver.m_functor;
+
+  const Kokkos::Experimental::SYCL& space = policy.space();
+  Kokkos::Experimental::Impl::SYCLInternal& instance =
+      *space.impl_internal_space_instance();
+  cl::sycl::queue& queue = *instance.m_queue;
+  queue.wait();
+
+  auto end   = policy.end();
+  auto begin = policy.begin();
+  cl::sycl::range<1> range(end - begin);
+
+  queue.submit([&](cl::sycl::handler& cgh) {
+    cgh.parallel_for(range, [=](cl::sycl::item<1> item) {
+      int id = item.get_linear_id();
+      functor(id);
+    });
+  });
+  queue.wait();
 }
 
 }  // namespace Impl
@@ -46,3 +47,4 @@ void sycl_launch(const Driver driver) {
 }  // namespace Kokkos
 
 #endif  // KOKKOS_SYCL_KERNELLAUNCH_HPP_
+
